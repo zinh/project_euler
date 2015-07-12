4a5d0f51c243c4c2e3c70ad8d5fc52efd8074a7d
Vinh Nguyen <xuanvinhct@gmail.com>
2015-07-12 15:03:27 +0000

    problem 8 finish

--- haskell/p008/p8.hs
+++ haskell/p008/p8.hs
@@ -1,17 +1,19 @@
 import Data.List
+import Data.Char
+import System.IO
 
---main = do
---  content <- readFile ("number.txt")
---  maximum (map lst_product $ foldl' (\acc x -> insert_char x acc) [] content)
+main = do
+  handle <- openFile "number.txt" ReadMode
+  content <- hGetContents handle
+  let m = maximum (map lst_product $ foldl' insert_char [] (init content))
+      in putStrLn (show m)
+  hClose handle
   
-insert_char :: a -> [[a]] -> [[a]]
-insert_char a [] = [[a]]
-insert_char a all@(x:xs)
+insert_char :: [[a]] -> a -> [[a]]
+insert_char [] a = [[a]]
+insert_char all@(x:xs) a
   | length x >= 13 = (a:(init x)): all
   | otherwise = (a:x):xs
   
 lst_product :: String -> Int
-lst_product lst = map convert_str lst
-
-convert_str :: String -> Int
-convert_str x = read x
+lst_product = product . map digitToInt
