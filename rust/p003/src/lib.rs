fff901cb41d831c40f36d14875d8dbea3e5b33fd
Vinh Nguyen <xuanvinhct@gmail.com>
2016-03-15 14:27:45 +0700

    problem 3 in rust

--- rust/p003/src/lib.rs
+++ rust/p003/src/lib.rs
@@ -1,3 +1,23 @@
-pub fn prime(n: i32) -> i32{
-    2
+pub fn prime(n: u64) -> u64{
+    let mut max_divisor = 0;
+    let mut m = n;
+    let mut i = 2;
+    loop {
+        if i > m {break;}
+        if (m % i) == 0{
+            m = m / i;
+            max_divisor = i;
+        }
+        i += 1;
+    }
+    if max_divisor == 0{
+        return n
+    }
+    else{
+        return max_divisor
+    }
+}
+
+fn main(){
+    prime(600_851_475_143);
 }
--- rust/p003/tests/lib.rs
+++ rust/p003/tests/lib.rs
@@ -1,8 +1,20 @@
 extern crate p003;
 #[test]
-fn it_works() {
+fn should_return_2_when_2() {
     assert_eq!(2, p003::prime(2));
+}
+
+#[test]
+fn should_return_3_when_3() {
     assert_eq!(3, p003::prime(3));
+}
+
+#[test]
+fn should_return_5_when_10() {
     assert_eq!(5, p003::prime(10));
-    assert_eq!(6857, p003::prime(600851475143));
+}
+
+#[test]
+fn should_calculate_with_big_number() {
+    assert_eq!(6857, p003::prime(600_851_475_143));
 }
