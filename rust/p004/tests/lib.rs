75cb7735ab3b038a243faab356221035c454f050
Vinh Nguyen <xuanvinhct@gmail.com>
2016-03-15 22:48:27 +0700

    finish problem 4

--- rust/p004/tests/lib.rs
+++ rust/p004/tests/lib.rs
@@ -8,4 +8,10 @@ fn check_palindrome_1() {
     assert_eq!(true, p004::check_palindrome(2002));
     assert_eq!(false, p004::check_palindrome(21));
     assert_eq!(false, p004::check_palindrome(2310));
+    assert_eq!(true, p004::check_palindrome(906609));
+}
+
+#[test]
+fn check_largest_palindrome(){
+    assert_eq!(906609, p004::largest_palindrome());
 }
